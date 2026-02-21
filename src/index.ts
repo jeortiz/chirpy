2b8a36bb5daaa742d1658e7b2e5448429e296572
Juan Ortiz <juan@JuanElias-MacBook-Pro.local>
2026-02-21 09:26:18 -0400

    adding app folder

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import express from "express";
+import { Response, Request } from "express"
 
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("."));
+const handlerReadiness = async function (req: Request, resp: Response): Promise<void> {
+    resp.status(200);
+    resp.set("Content-Type", "text/plain; charset=utf-8");
+    resp.send("OK")
+}
+
+app.use("/app", express.static("./src/app"));
+
+app.get("/healthz", handlerReadiness);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);

b690830cafc76b2340b060e010e54d660846fef4
Juan Ortiz <juan@JuanElias-MacBook-Pro.local>
2026-02-27 18:19:19 -0400

    configuring drizzle and users table

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,19 @@
 type APIConfig = {
     fileServerHits: number
+    dbURL: string
 }
 
-export const config: APIConfig = { fileServerHits: 0}
+process.loadEnvFile("../.env");
+
+export const config: APIConfig = { 
+    fileServerHits: 0,
+    dbURL: envOrThrow("DB_URL"),
+}
+
+function envOrThrow(key: string): string {
+    if (!(key in process.env)) {
+        throw new Error;
+    }
+
+    return process.env[key] as string
+}
